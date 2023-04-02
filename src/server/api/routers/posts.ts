b6db4e7400b1a3deb32dc5f3eff3d9b3fbbb39f7
Chimmy <jimmyeetran@gmail.com>
2023-04-02 19:09:38 +1000

    forgot to add Type to { Users }

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,5 @@
-import { User } from "@clerk/nextjs/dist/api";
+// For some reason this has to be a type import
+import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
@@ -7,11 +8,11 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 // using this to filter out the data returned from usersList
 const filterUserForClient = (user: User) => {
   return {
-    id: user.id, 
+    id: user.id,
     username: user.username,
     profileImageUrl: user.profileImageUrl,
   };
-}
+};
 
 export const postsRouter = createTRPCRouter({
   // Apparently this is a promise query
@@ -24,10 +25,12 @@ export const postsRouter = createTRPCRouter({
       take: 100,
     });
 
-    const users = (await clerkClient.users.getUserList({
-      userId: posts.map((post) => post.authorId),
-      limit: 100,
-    })).map(filterUserForClient);
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
 
     console.log(users);
 
